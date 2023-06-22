f12446e50b1783fca588d74a50938962cfe179d4
Andrey Khokhlov <kron-k@ya.ru>
2023-06-22 23:56:26 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное число
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32( Console.ReadLine() );
+
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32( Console.ReadLine() );
+
+Console.WriteLine("Максимальное число: " + Math.Max(num1, num2));
